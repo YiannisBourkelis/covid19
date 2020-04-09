6acafd473df0b41e60bdeecd060225f16e2c285d
Yiannis Bourkelis <yiannis1234@gmail.com>
2020-04-09 23:48:32 +0300

    Update statistics.blade.php

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -10,7 +10,7 @@
 Επιμέλεια στατιστικών: Στάθης Γρηγοριάδης - <a href="https://covid19gr.wordpress.com">https://covid19gr.files.wordpress.com</a>
 </div>
 
-<img src="https://covid19gr.files.wordpress.com/2020/04/covid08-04-2020-1.png" class="img-fluid" alt="Responsive image">
+<img src="https://covid19gr.files.wordpress.com/2020/04/covid09-04-2020-1.png" class="img-fluid" alt="Responsive image">
 
 </div>
 </div>
