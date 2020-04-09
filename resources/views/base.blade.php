6e71ec0d4826551b09d9203455caa2b809a740a9
Yiannis Bourkelis <yiannis1234@gmail.com>
2020-04-09 05:08:20 +0300

    change jquery cdn

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -12,7 +12,7 @@
   <title>COVID-19</title>
 
   <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.slim.min.js"></script>
 </head>
 <body>
   <div class="container">
